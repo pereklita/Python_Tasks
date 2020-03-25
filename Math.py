dbfb6998e27719ca1e5099cbc0911581052ffd97
Ihor Pereklita <ihorpereklita@gmail.com>
2020-03-25 18:15:15 +0200

    Start

---
+++ Math.py
@@ -0,0 +1,7 @@
+number_a = int(input ("Please enter your number a = "))
+number_b = int(input ("Please enter your number b = "))
+print("sum =",number_a+number_b)
+print("sub =",number_a-number_b)
+print("mult =",number_a*number_b)
+print("div =",number_a/number_b)
+
