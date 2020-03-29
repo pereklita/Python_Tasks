d8c55a170aef0872c7b79ee4bfa2396f35f09284
Ihor Pereklita <ihorpereklita@gmail.com>
2020-03-29 21:39:29 +0300

    Classwork

---
+++ classwork.py
@@ -0,0 +1,16 @@
+#Порівняння двох чисел
+a=int(input("enter your number a "))
+b=int(input("enter your number b "))
+if a>b:
+    print("a>b")
+else:
+    print ("a<b")
+#Перевірка на парність числа
+a = int(input("Enter_your_number "))
+if a%2==0:
+    print("Парне")
+else:
+    print("Непарне")  
+#Знаходження факторіалу         
+a=int(input("Enter_number_a: "))
+print ("Factorial of number a : ",math.factorial(a))
