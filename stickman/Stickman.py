8e87f95cd4f8fbf1c4f01aeeef588858abc44c99
Ihor Pereklita <ihorpereklita@gmail.com>
2020-04-30 18:31:05 +0300

    Game

--- stickman/Stickman.py
+++ stickman/Stickman.py
@@ -68,18 +68,12 @@ def collided_right(co1, co2):
             return True 
     return False
 
-def colided_top(co1, co2):
+def collided_top(co1, co2):
     if within_x(co1, co2):
         if co1.y1 <= co2.y2 and co1.y1 >= co2.y1:
             return True
     return False 
-
-def collided_top(co1, co2):
-    if within_x(co1, co2): 
-        if co1.y1 <= co2.y2 and co1.y1 >= co2.y1:
-            return True
-    return False
-
+    
 def collided_bottom(y, co1, co2):
     if within_x(co1,co2):
         y_calc = co1.y2 + y
